4832e626b2b6777b34f1c74ab54e431ac739ad92
cyee00 <cyee00@stuy.edu>
2018-09-26 23:19:47 -0400

    Commit #3

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -7,20 +7,21 @@ public class ArrayDemo{
     System.out.print(ans+ary[ary.length-1]+"]");
   }
   public static void printArray(int[][]ary){
-    String ans = "[";
-    for (int i =0;i<ary.length-1;i++) {
-      ans += ary[i] + ", ";
+    String ans1 = "[";
+    for (int i =0;i<ary[1].length-1;i++) {
+      ans1 += ary[1][i] + ", ";
     }
-    System.out.print(ans+ary[ary.length-1]+"]");
-    for (int i =0;i<ary.length-1;i++) {
-      ans += ary[i] + ", ";
+    System.out.print(ans1+ary[ary[1].length-1]+"]");
+    String ans2 = "[";
+    for (int i =0;i<ary[2].length-1;i++) {
+      ans2 += ary[2][i] + ", ";
     }
-    System.out.print(ans+ary[ary.length-1]+"]");
+    System.out.print(ans2+ary[ary[2].length-1]+"]");
   }
   public static void main(String[] args){
-    int[]ary=new int[5];
-    int[][]2dAry=new int[2][];
+    int[] ary=new int[5];
+    int[][] ary2 = new int[2][3];
     printArray(ary);
-    printArray(2dAry);
+    printArray(ary2);
   }
 }
