2bc7937a57ae36d1bfcc3774646f5692bee38bdb
cyee00 <cyee00@stuy.edu>
2018-09-28 00:04:24 -0400

    countZeros2D

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -4,18 +4,38 @@ public class ArrayDemo{
     for (int i =0;i<ary.length-1;i++) {
       ans += ary[i] + ", ";
     }
-    System.out.println(ans+ary[ary.length-1]+"}");
+    System.out.print(ans+ary[ary.length-1]+"}");
   }
   public static void printArray(int[][]ary){
     System.out.print("{");
     printArray(ary[0]);
+    System.out.print("\n");
     printArray(ary[1]);
     System.out.print("}");
   }
+  public static int countZeros2D(int[][] nums){
+    int ans = 0;
+    for (int i=0;i<nums[0].length;i++) {
+      if (nums[0][i]==0) {
+        ans++;
+      }
+    }
+    for (int i=0;i<nums[1].length;i++) {
+      if (nums[1][i]==0) {
+        ans++;
+      }
+    }
+    return ans;
+  }
   public static void main(String[] args){
     int[] array=new int[5];
-    int[][] array2 = new int[2][3];
+    int[][] array2 = new int[][] {
+      {0,0,3,0},
+      {0,1,2,3},
+    };
     printArray(array);
+    System.out.print("\n");
     printArray(array2);
+    System.out.print("\n"+countZeros2D(array2));
   }
 }
