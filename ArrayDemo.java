9401f006c5e2eb8beb9e38b9ed72751dc1523535
cyee00 <cyee00@stuy.edu>
2018-09-28 07:49:50 -0400

    Everything working

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -45,13 +45,11 @@ public class ArrayDemo{
   public static int[][] fill2DCopy(int[][] vals){
     int[][] copiedArray = new int[vals.length][vals[0].length];
     for (int i=0;i<vals[0].length;i++) {
-      System.out.print(vals[0][i]);
       if (vals[0][i]<0) {
         copiedArray[0][i]=3;
       } else copiedArray[0][i]=1;
     }
     for (int i=0;i<vals[1].length;i++) {
-      System.out.print(vals[1][i]);
       if (vals[1][i]<0) {
         copiedArray[1][i]=3;
       } else copiedArray[1][i]=1;
@@ -61,15 +59,19 @@ public class ArrayDemo{
   public static void main(String[] args){
     int[] array=new int[5];
     int[][] array2 = new int[][] {
+      {0,0,3,0},
+      {0,1,2,0},
+    };
+    int[][] array3 = new int[][] {
       {0,-5,3,0},
       {0,1,2,-2},
     };
     printArray(array);
-    System.out.print("\n");
+    System.out.print("\n\n");
     printArray(array2);
-    System.out.print("\n"+countZeros2D(array2)+"\n");
+    System.out.print("\n\n"+countZeros2D(array2)+"\n\n");
     fill2D(array2);
-    System.out.print("\n");
-    printArray(fill2DCopy(array2));
+    System.out.print("\n\n");
+    printArray(fill2DCopy(array3));
   }
 }
